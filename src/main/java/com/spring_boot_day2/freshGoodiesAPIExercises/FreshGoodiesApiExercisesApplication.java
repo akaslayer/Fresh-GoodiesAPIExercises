3a39ac80f4b687afd73b3024c6c515edc8dab1ea
akaslayer <hendrysurijanto123@gmail.com>
2024-05-29 09:30:10 +0700

    first commit

---
+++ src/main/java/com/spring_boot_day2/freshGoodiesAPIExercises/FreshGoodiesApiExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_day2.freshGoodiesAPIExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshGoodiesApiExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshGoodiesApiExercisesApplication.class, args);
+	}
+
+}
